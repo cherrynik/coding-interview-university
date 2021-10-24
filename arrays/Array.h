67efa1f49fb14c5228af098a6a54e08b7791b0be
Nikolay Cherednik <32453685+cherrynik@users.noreply.github.com>
2021-10-24 13:21:52 +0300

    Inline modifier removed. No effect.

--- arrays/Array.h
+++ arrays/Array.h
@@ -24,7 +24,7 @@ private:
   void save_and_move_when(Array::Operation operation,
                           value_type input,
                           int start_from_i);
-  inline value_type* at_memory(int i) const;
+  value_type* at_memory(int i) const;
 
 public:
   Array(int size = 1);
